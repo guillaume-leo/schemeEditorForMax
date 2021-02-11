2c3228e4f55a15aaacd7ca100f7c0c6b0e84082d
guillaume-leo <quet.guillaume.leo@gmail.com>
2021-02-11 18:22:46 +0100

    some changes

---
+++ script.js
@@ -0,0 +1,76 @@
+var cm = new CodeMirror.fromTextArea(document.getElementById("editor"), {
+  autoRefresh:true,
+  scrollbarStyle:'overlay',
+  lineNumbers: true,
+  tabSize: 4,
+  mode:'scheme',
+  theme:'night',
+  gutters: ["CodeMirror-linenumbers", "breakpoints"],
+  autoCloseBrackets: true,
+  matchBrackets:true,
+  styleActiveLine: {nonEmpty: true},
+  styleActiveSelected: true,
+  extraKeys: {
+    "Esc": function(cm) {
+      cm.setOption("fullScreen", !cm.getOption("fullScreen"));
+    },
+    "Ctrl-Enter": () => { 
+      var cur = cm.getCursor().line;
+      var arr = cm.getValue().split("\n");
+      var txt = '', start = 0, stop = 0;
+      for (i = 0; i < arr.length; i++) {
+          t = arr[i].trim();
+          len = t.length;
+          if (!start) {
+              if (!len) txt = '';
+              if (i == cur) start = 1;
+          }
+          if (!stop) {
+              if (start && !len) stop = 1;
+              else if (len) txt += t + "\n";
+          }
+      }
+      getBlock(cur, arr);
+    }
+  }
+});
+
+
+
+
+// set the size of the editor
+cm.setSize(600, 600);
+
+
+function getBlock(pos , arr){
+  currentPos = pos;
+  array = arr;
+  out = [];
+  while(array[currentPos] !== "" && currentPos >= 0){
+    currentPos --;
+  }
+  out.push(currentPos+1);
+  currentPos = pos;
+  while(array[currentPos] !== "" && array[currentPos] !== undefined){
+    currentPos ++;
+  }
+  out.push(currentPos-1);
+  for (var i =out[0]; i<=out[1];i++){
+    blink(i);
+  }
+}
+
+
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
+async function blink (n) { 
+  for (var i = 0; i < 2; i++) {
+    if (i==0){cm.addLineClass(n, 'wrap', 'eval');}else{cm.removeLineClass(n, 'wrap', 'eval');}
+    await timer(500); 
+  }
+}
+
+
+
+
+
